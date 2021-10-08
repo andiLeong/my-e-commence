90bac5c3e55af36a59865ba084c24822850a21cb
fortitudinous <fortitudinous@126.com>
2021-10-08 17:58:30 +0800

    fix add to cart not login bug

--- app/Http/Livewire/AddToCart.php
+++ app/Http/Livewire/AddToCart.php
@@ -14,6 +14,9 @@ class AddToCart extends Component
 
     public function addToCart($product_id)
     {
+        if( !auth()->check()){
+            return $this->dispatchBrowserEvent('notification', ['msg' => 'Please Login First!' , 'type' => 'danger']);
+        }
         Product::findOrFail($product_id);
         (new Cart)->addBy(auth()->user(),['quantity' => 1 , 'product_id' => $product_id]);
         $refresh;
